0cd829157833d4e548eb8e0117aa151e048629c5
Aditya <adityaraj6311@gmail.com>
2022-06-19 12:21:08 +0530

    .

---
+++ AeroRobot/modules/start.py
@@ -0,0 +1,47 @@
+import asyncio
+
+from telegram.ext import CommandHandler
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(CommandHandler("start") & filters.private & ~filters.group & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
+    await message.reply_photo(
+        photo=f"https://telegra.ph//file/9e38bddf52338e9142923.jpg",
+        caption=f"""**━━━━━━━━━━━━━━━━━━
+💔 ʜᴇʏ {message.from_user.mention()} !
+
+        ᴛʜɪs ɪs [Aero ✘ Management~🇮🇳](t.me/Aero_Management_Bot), ᴀ sᴜᴘᴇʀ ғᴀsᴛ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘ...
+
+ᴀʟʟ ᴏꜰ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʜᴀɴᴅʟᴇʀs : ( `/ . ! ` )
+┏━━━━━━━━━━━━━━┓
+┣★
+┣★ ᴍᴀᴅᴇ ʙʏ: [🇮🇳AerodynamicV1OFFICIAL🇮🇳](t.me/AerodynamicV1_OFFICIAL)
+┣★
+┗━━━━━━━━━━━━━━┛
+
+💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴀʙᴏᴜᴛ ᴍᴇ ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](t.me/AerodynamicV1_OFFICIAL)...
+━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🥺 ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ​ 🥺", url=f"https://t.me/Aero_Management_Bot?startgroup=true"
+                       ),
+                  ],[
+                    InlineKeyboardButton(
+                        "💔 ᴏᴡɴᴇʀ 💔", url=f"https://t.me/AerodynamicV1_OFFICIAL"
+                    ),
+                    InlineKeyboardButton(
+                        "🍒 sᴜᴘᴘᴏʀᴛ 🍒", url=f"https://t.me/AerodynamicV1_Promotion"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        "🔎 ɪɴʟɪɴᴇ 🔎", switch_inline_query_current_chat=""
+                    ),
+                    ]
+            ]
+       ),
+    )
